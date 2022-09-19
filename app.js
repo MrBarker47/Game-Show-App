5a9d4ce98c13341a1280efde92d8bfe714b55bfb
MrBarker47 <jebarker7@gmail.com>
2022-09-19 15:06:49 -0400

    Updated files

---
+++ app.js
@@ -0,0 +1,8 @@
+const qwerty = document.getElementById('qwerty');
+const pharase = document.getElementById('phrase');
+const button = document.getElementsByClassName('btn_reset');
+
+
+button.addEventListener('click', () => {
+
+});

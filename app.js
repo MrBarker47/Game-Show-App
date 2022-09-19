1a7d69e5c27795510ec25387fdc9ad2d1bbf99e0
MrBarker47 <jebarker7@gmail.com>
2022-09-19 19:10:03 -0400

    updated files

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+//Getting the elements by their ID's and Classes
 const qwerty = document.getElementById('qwerty');
 const pharase = document.getElementById('phrase');
 const button = document.getElementsByClassName('btn_reset');
+let playerGuesses;
 
+for(let i = 0; i < playerGuesses.length; i++) {
+    
+}
 
 button.addEventListener('click', () => {
 

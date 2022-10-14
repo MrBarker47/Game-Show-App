2a57b93462273b1ac8c87b2d2f0146da16b25917
MrBarker47 <jebarker7@gmail.com>
2022-10-14 18:38:14 -0400

    update files

--- app.js
+++ app.js
@@ -3,39 +3,38 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const button = document.querySelector('.btn__reset');
 const divOne = document.getElementById('overlay');
+// const newDiv = document.querySelector('ul');
 let missed = 0;
 
 
 
-const phrases = ['Hello World', 'Only The Strong Survive', 'Work Hard Play Hard',
-'EveryBody Eats', 'You Wear The Crown'];
+const phrases = ['hello world', 'only the strong survive', 'work hard play hard',
+'everybody eats', 'you wear the crown'];
 
 // Get a random phrases from the array that you created
-function getRandomPhrasesArray(array) {
-const randomNumber = Math.floor(Math.random() * array.length);
-   let newWord =  array[randomNumber].split('');
-    let word = phrases[0];
-    return phrases;
-}
-
-function addPhraseToDisplay(array) {
-    for (let i = 0; i < array.length; i++) {
-    const li = document.createElement('li');
-      li.textContent = 'array[i]';
-      li.classList.add('letter');
-      li.append('ul');
-      break;
-    }
-
-}
+// function getRandomPhrasesArray(array) {
+// const randomNumber = Math.floor(Math.random() * array.length);
+//    let newWord =  array[randomNumber].split('');
+//     let word = phrases[0];
+//     return phrases;
+// }
 
 
+// function addPhraseToDisplay(array) {
+//     for (let i = 0; i < array.length; i++) {
+//     const li = document.createElement('li');
+//       li.textContent = 'array[i]';
+//       li.className('letter');
+//        newDiv.appendChild(li);
+//        break;
+//     }
+
+// }
 
 
 
-button.addEventListener('click', () => {
-    console.log('clicked');
-    overlay.style.display = 'none';
-    getRandomPhrasesArray(phrases);
-  });
+// button.addEventListener('click', () => {
+//     overlay.style.display = 'none';
+//     getRandomPhrasesArray(phrases);
+//   });
 

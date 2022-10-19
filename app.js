9d8327a3006ec9e1438efec0af251b54ce73a0a3
MrBarker47 <jebarker7@gmail.com>
2022-10-19 18:02:09 -0400

    updated files

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const button = document.querySelector('.btn__reset');
 const divOne = document.getElementById('overlay');
+const newDiv = document.getElementsByTagName('ul');
 let missed = 0;
 
 
@@ -13,15 +14,16 @@ const phrases = ['hello world', 'only the strong survive', 'work hard play hard'
 function getRandomPhrasesArray(array) {
   const randomNumber = Math.floor(Math.random() * array.length);
     let newWord = array[randomNumber].split('');
-     return array[randomNumber];
+     return array[randomNumber].split('');
 }
 
 function addPhraseToDisplay(array) {
   for (let i = 0; i < array.length; i++) {
     const li = document.createElement('li');
      li.textContent = 'array[i]';
-     li.classList.add('letter');
-     
+      li.classList.add('letter');
+       newDiv.append('li');
+       break;
    }
 }
 
